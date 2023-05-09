5a67b2588348650f1900a5fe4254c6c757140afc
venkat <venkat.mobiqa@gmail.com>
2023-05-09 10:41:01 +0530

    updating ./tf-module-vpc2

--- main.tf
+++ main.tf
@@ -8,15 +8,14 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "main" {
-  count = length(var.subnets_cidr)
+  count      = length(var.subnets_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.subnets_cidr[count.index]
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-subnet-$[count.index + 1}" }
+    { Name = "${var.env}-subnet-${count.index + 1}" }
   )
-
 }
 
 resource "aws_vpc_peering_connection" "peer" {

b001b807818dc89be892d78d734fe7874cb4536d
venkat <venkat.mobiqa@gmail.com>
2023-05-11 12:09:28 +0530

    updating ./tf-module-vpc2

--- main.tf
+++ main.tf
@@ -18,6 +18,19 @@ resource "aws_subnet" "public" {
   )
 }
 
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets_cidr[count.index]
+
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-private_subnet-${count.index + 1}" }
+  )
+}
+
+
 
 resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
@@ -30,35 +43,6 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
-
-resource "aws_route" "default" {
-  route_table_id            = aws_vpc.main.default_route_table_id
-  destination_cidr_block    = "172.31.0.0/16"
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
-resource "aws_route" "igw-route" {
-  route_table_id            = aws_vpc.main.default_route_table_id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
-}
-
-
-resource "aws_route" "default-vpc" {
-  route_table_id            = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block    = var.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-igw" }
-  )
-}
-#
 #resource "aws_route_table" "public" {
 #  vpc_id = aws_vpc.main.id
 #
@@ -83,6 +67,36 @@ resource "aws_internet_gateway" "igw" {
 #  subnet_id      = aws_subnet.public.*.id[count.index]
 #  route_table_id = aws_route_table.public.id
 #}
+
+
+#resource "aws_route" "default" {
+#  route_table_id            = aws_vpc.main.default_route_table_id
+#  destination_cidr_block    = "172.31.0.0/16"
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#}
+#
+#resource "aws_route" "igw-route" {
+#  route_table_id            = aws_vpc.main.default_route_table_id
+#  destination_cidr_block    = "0.0.0.0/0"
+#  gateway_id = aws_internet_gateway.igw.id
+#}
+#
+#
+#resource "aws_route" "default-vpc" {
+#  route_table_id            = data.aws_vpc.default.main_route_table_id
+#  destination_cidr_block    = var.cidr_block
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-igw" }
+  )
+}
+#
 #
 #resource "aws_eip" "ngw-eip" {
 #  vpc = true
@@ -129,46 +143,46 @@ resource "aws_internet_gateway" "igw" {
 #
 #
 #
-// Create EC2
-data "aws_ami" "centos8" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.centos8.id
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  subnet_id              = aws_subnet.main.*.id[0]
-
-  tags = {
-    Name = "test-centos8"
-  }
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
+#// Create EC2
+#data "aws_ami" "centos8" {
+#  most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#  owners      = ["973714476881"]
+#}
+#
+#resource "aws_instance" "web" {
+#  ami                    = data.aws_ami.centos8.id
+#  instance_type          = "t3.micro"
+#  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#  subnet_id              = aws_subnet.main.*.id[0]
+#
+#  tags = {
+#    Name = "test-centos8"
+#  }
+#}
+#
+#resource "aws_security_group" "allow_tls" {
+#  name        = "allow_tls"
+#  description = "Allow TLS inbound traffic"
+#  vpc_id      = aws_vpc.main.id
+#
+#  ingress {
+#    description = "TLS from VPC"
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#
+#  tags = {
+#    Name = "allow_tls"
+#  }
+#}

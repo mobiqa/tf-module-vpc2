db17ddf27da170b80b817f7c3bf6370a7157821e
venkat <venkat.mobiqa@gmail.com>
2023-05-11 10:23:35 +0530

    updating ./tf-module-vpc2

--- main.tf
+++ main.tf
@@ -30,6 +30,22 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
+
+resource "aws_route" "default" {
+  route_table_id            = aws_vpc.main.default_route_table_id
+  destination_cidr_block    = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+
+
+
 #resource "aws_internet_gateway" "igw" {
 #  vpc_id = aws_vpc.main.id
 #
@@ -105,12 +121,7 @@ resource "aws_vpc_peering_connection" "peer" {
 #  route_table_id = aws_route_table.private.id
 #}
 #
-#resource "aws_route" "r" {
-#  route_table_id            = data.aws_vpc.default.main_route_table_id
-#  destination_cidr_block    = var.cidr_block
-#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-#}
-#
+
 #
 #
 #

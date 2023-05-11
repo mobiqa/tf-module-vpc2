66b245048afbb34eee3a5a89a1d3adb6b0bf5934
venkat <venkat.mobiqa@gmail.com>
2023-05-11 09:35:37 +0530

    updating ./tf-module-vpc2

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   count = length(var.subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnets_cidr
+  cidr_block = var.subnets_cidr[count.index]
 
 
   tags = merge(

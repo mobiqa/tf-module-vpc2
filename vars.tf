b059d027afdf5f26244d46b98f1acde1c9d0b131
venkat <venkat.mobiqa@gmail.com>
2023-05-11 09:23:37 +0530

    updating ./tf-module-vpc2

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "cidr_block" {}
 variable "env" {}
-subnets_cidr {}
+variable subnets_cidr {}
 #variable "private_subnets_cidr" {}
 #variable "default_vpc_id" {}
 #variable "availability_zones" {}

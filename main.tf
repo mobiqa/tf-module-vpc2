71bf09c2fa86b8bf02566bfe49b67db461e64413
venkat <venkat.mobiqa@gmail.com>
2023-05-11 09:50:47 +0530

    updating ./tf-module-vpc2

--- main.tf
+++ main.tf
@@ -14,45 +14,22 @@ resource "aws_subnet" "main" {
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-${count.index + 1}" }
+    { Name = "${var.env}-subnet-${count.index + 1}" }
   )
 }
 
-#resource "aws_subnet" "public" {
-#  count      = length(var.public_subnets_cidr)
-#  vpc_id     = aws_vpc.main.id
-#  cidr_block = var.public_subnets_cidr[count.index]
-#  availability_zone = var.availability_zones[count.index]
-#
-#
-#  tags = merge(
-#    local.common_tags,
-#    { Name = "${var.env}-public-subnet-${count.index + 1}" }
-#  )
-#}
-#
-#resource "aws_subnet" "private" {
-#  count      = length(var.private_subnets_cidr)
-#  vpc_id     = aws_vpc.main.id
-#  cidr_block = var.private_subnets_cidr[count.index]
-#
-#  tags = merge(
-#    local.common_tags,
-#    { Name = "${var.env}-private-subnet-${count.index + 1}" }
-#  )
-#}
-#
-#resource "aws_vpc_peering_connection" "peer" {
-#  peer_owner_id = data.aws_caller_identity.current.account_id
-#  peer_vpc_id   = var.default_vpc_id
-#  vpc_id        = aws_vpc.main.id
-#  auto_accept   = true
-#  tags = merge(
-#    local.common_tags,
-#    { Name = "${var.env}-peering" }
-#  )
-#}
-#
+
+resource "aws_vpc_peering_connection" "peer" {
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id   = var.default_vpc_id
+  vpc_id        = aws_vpc.main.id
+  auto_accept   = true
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-peering" }
+  )
+}
+
 #resource "aws_internet_gateway" "igw" {
 #  vpc_id = aws_vpc.main.id
 #

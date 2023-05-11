ba6f010c2efd314152765178f77c6c9bbfaa53eb
venkat <venkat.mobiqa@gmail.com>
2023-05-11 14:47:44 +0530

    updating ./tf-module-vpc2

---
+++ mn.tf
@@ -0,0 +1,57 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-vpc" }
+  )
+}
+
+resource "aws_vpc_peering_connection" "peer" {
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id   = var.default_vpc_id
+  vpc_id        = aws_vpc.main.id
+  auto_accept   = true
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-peering" }
+  )
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-igw" }
+  )
+}
+
+
+
+resource "aws_eip" "ngw-eip" {
+  vpc = true
+}
+
+#resource "aws_nat_gateway" "ngw" {
+#  allocation_id = aws_eip.ngw-eip.id
+#  subnet_id     = aws_subnet.public.*.id[0]
+#
+#  tags = merge(
+#    local.common_tags,
+#    { Name = "${var.env}-ngw" }
+#  )
+#
+#  //depends_on = [aws_internet_gateway.example]
+#}
+#
+
+resource "aws_route" "r" {
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+
+
+
+

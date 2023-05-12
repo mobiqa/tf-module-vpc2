8ccdc34c4dfd10021e481ea9d6c9c8075f5af1b2
venkat <venkat.mobiqa@gmail.com>
2023-05-12 18:11:15 +0530

    updating ./tf-module-vpc2

--- outputs.tf
+++ outputs.tf
@@ -6,9 +6,7 @@ output "vpc_peering_connection_id" {
   value = aws_vpc_peering_connection.peer.id
 }
 
-#output "internet_gw_id" {
-#  value = aws_internet_gateway.igw.id
-#}
+
 output "public_subnet_ids" {
   value = module.public_subnets
 }

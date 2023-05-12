a2040a74a7916c6339629a4239debfc6d99719ef
venkat <venkat.mobiqa@gmail.com>
2023-05-12 09:16:21 +0530

    updating ./tf-module-vpc2

--- data.tf
+++ data.tf
@@ -3,3 +3,5 @@
 data "aws_vpc" "default" {
   id = var.default_vpc_id
 }
+
+data "aws_caller_identity" "current" {}
---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "vpc_peering_connection_id" {
+  value = aws_vpc_peering_connection.peer.id
+}
+
+#output "public_subnet_ids" {
+#  value = module.public_subnets
+#}
+#
+#output "private_subnet_ids" {
+#  value = module.private_subnets
+#}

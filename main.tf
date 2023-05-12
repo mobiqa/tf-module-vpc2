4886057fbd0c849495592fcf315b6cb2edf57599
venkat <venkat.mobiqa@gmail.com>
2023-05-12 13:22:41 +0530

    updating ./tf-module-vpc2

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags = merge(
@@ -18,41 +17,9 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-igw" }
-  )
-}
-
-
-
-resource "aws_eip" "ngw-eip" {
-  vpc = true
-}
-
-#resource "aws_nat_gateway" "ngw" {
-#  allocation_id = aws_eip.ngw-eip.id
-#  subnet_id     = aws_subnet.public.*.id[0]
-#
-#  tags = merge(
-#    local.common_tags,
-#    { Name = "${var.env}-ngw" }
-#  )
-#
-#  //depends_on = [aws_internet_gateway.example]
-#}
-#
-
 resource "aws_route" "r" {
   route_table_id            = data.aws_vpc.default.main_route_table_id
   destination_cidr_block    = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
-
-
-
-
 
